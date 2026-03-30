0d60cff862341dbf10c4864b2c39437702a52173
emieshanatasha122-max <emieshanatasha122@gmail.com>
2026-03-30 16:12:55 +0800

    Update Globe3D.tsx

--- src/components/Globe3D.tsx
+++ src/components/Globe3D.tsx
@@ -281,11 +281,9 @@ export default function Globe3D({ data }: Globe3DProps) {
   // Globe textures
   const globeImageUrl = basemap === 'satellite'
   ? (isDark
-      ? '//unpkg.com/three-globe/example/img/earth-night.jpg'
-      : '//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-  : (isDark
-      ? '/assets/Political-World-Map-Dark.jpg' // Fail yang anda upload (versi gelap)
-      : '/assets/Political-World-Map-3360.jpg'); // Fail yang anda upload
+      ? '//unpkg.com/three-globe/example/img/earth-night.jpg'           
+      : '//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')    
+  : 'https://www.nationsonline.org/maps/Political-World-Map-3360.jpg';    
 
   const atmosphereColor = isDark ? '#06B6D4' : '#87CEEB';
 
